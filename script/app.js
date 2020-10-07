7f8f8e01a6c5156c1de532596f4593a732327f1b
DepreesterEwoud <ewoud.de.preester@student.howest.be>
2020-10-07 10:45:16 +0200

    PasswordCheck

--- script/app.js
+++ script/app.js
@@ -1,13 +1,39 @@
-function handleFloatingLabel() {
-   
+let email = {},
+    password = {},
+    signInButton;
+function getDOMElements(){
+    
 }
+/* function handleFloatingLabel(){
+
+} */
+
 
 function handlePasswordSwitcher() {
     
+    let checkbox = document.querySelector(`.c-toggle-password__checkbox`);
+    let passwordInput = document.querySelector(`.c-toggle-password__input`);
+    checkbox.addEventListener(`change`, function(){
+
+        if(checkbox.checked){
+            passwordInput.type = "text";
+            
+            console.log("unchecked")
+        }
+        else{
+            passwordInput.type = "password";
+            console.log("checked")
+        }
+        
+
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
-    handleFloatingLabel();
+    getDOMElements();
+    //handleFloatingLabel();
     handlePasswordSwitcher();
 });
